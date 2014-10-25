dc323c28904dac3d0566cbbeaa4e7a41fb0569eb
j5r5myk <j5r5myk@gmail.com>
2014-10-25 17:37:59 -0400

    outfit constructor

--- objective_fashion/src/Outfit.java
+++ objective_fashion/src/Outfit.java
@@ -10,12 +10,29 @@ import clothing.generics.Tops;
 
 
 public class Outfit {
-	Tops top;
-	Bottoms bottom;
-	Socks socks;
-	Shoes shoes;
-	Outerwear outerwear;
-	Singletons singleton;
-	ArrayList<Accessories> accessories;
+	Tops theTop;
+	Bottoms theBottom;
+	Socks theSocks;
+	Shoes theShoes;
+	Outerwear theOuterwear;
+	Singletons theSingleton;
+	ArrayList<Accessories> theAccessories; //array list to add multiple accessories
+	
+	
+	//constructor passes already created objects from wardrobe
+	public Outfit(Tops top, Bottoms bottom, Socks sock, Shoes shoes, Outerwear outerwear, Singletons singleton, Accessories[] accessories) {
+		theTop=top;
+		theBottom=bottom;
+		theSocks=sock;
+		theShoes=shoes;
+		theOuterwear=outerwear;
+		theSingleton=singleton;
+	//accessories loop to add all items
+		for (int i=0; i < accessories.length; i++) {
+			theAccessories.add(accessories[i]);
+		}
+		
+	}
+	
 	
 }
