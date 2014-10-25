df175b0ad7bd16fd6fa3afd8c6662685e9a47dd0
iambrett3 <brett.reuben@gmail.com>
2014-10-25 17:55:17 -0400

    Brett 5:55

--- objective_fashion/src/controller/TheDecider.java
+++ objective_fashion/src/controller/TheDecider.java
@@ -1,5 +1,14 @@
 package controller;
 
+import clothing.generics.Accessories;
+import clothing.generics.Bottoms;
+import clothing.generics.Outerwear;
+import clothing.generics.Shoes;
+import clothing.generics.Singletons;
+import clothing.generics.Socks;
+import controller.State.TemperatureState;
+import clothing.tops.*;
+
 public class TheDecider {
 	State state;
 	
@@ -10,6 +19,28 @@ public class TheDecider {
 	 * @return
 	 */
 	public Outfit decideOutfit(Preferences preferences, Wardrobe wardrobe) {
-		return new Outfit();
+		TemperatureState temperature = state.getTemperatureState();
+		switch (temperature) {
+		case VERY_COLD: return createColdOutfit(); //these method calls are very abstract for now 
+		break;
+		case COLD: return createColdOutfit();  
+		break;
+		case COOL: return createColdOutfit(); 
+		break;
+		case WARM: return createHotOutfit(); 
+		break;
+		case HOT: return createHotOutfit();  
+		break;
+		case VERY_HOT: return createHotOutfit(); 
+		break;
+		}
+	}
+	
+	public Outfit createColdOutfit() {
+		return new Outfit(new Bomber(), Bottoms bottom, Socks sock, Shoes shoes, Outerwear outerwear, Singletons singleton, Accessories[] accessories)
+	}
+	
+	public Outfit createHotOutfit() {
+		
 	}
 }
