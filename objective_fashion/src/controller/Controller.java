a74c05162ab520dd23e7b75e92bf922ba7bee5f7
iambrett3 <brett.reuben@gmail.com>
2014-10-25 20:05:16 -0400

    added main method in Controller

--- objective_fashion/src/controller/Controller.java
+++ objective_fashion/src/controller/Controller.java
@@ -1,6 +1,14 @@
 package controller;
 
 import clothing.Outfit;
+import clothing.accessories.*;
+import clothing.bottoms.*;
+import clothing.generics.*;
+import clothing.outerwear.*;
+import clothing.shoes.*;
+import clothing.singletons.*;
+import clothing.tops.*;
+import clothing.variables.Formality;
 import userProfile.Preferences;
 import userProfile.UserProfile;
 import userProfile.Wardrobe;
@@ -12,10 +20,26 @@ import userProfile.Wardrobe;
 public class Controller {
 	TheDecider decider;
 	UserProfile profile;
+	Wardrobe wardrobe; //these field should ultimately be contained in profile. just for testing
+	Preferences preferences;
 	
 	
 	public Controller() {
 		decider = new TheDecider();
+		wardrobe = new Wardrobe();
+		preferences = new Preferences(Formality.CASUAL);
+		randomPopulateWardrobe();
+		profile = new UserProfile(wardrobe, preferences);
+	}
+	
+	public void randomPopulateWardrobe() {
+		wardrobe.addAccessories(new Ring());
+		wardrobe.addBottoms(new Pants());
+		wardrobe.addOuterwear(new Bomber());
+		wardrobe.addShoes(new Boots());
+		wardrobe.addSingletons(new Romper());
+		wardrobe.addSocks(new Socks());
+		wardrobe.addTops(new Hoodie());
 	}
 	
 	/**
@@ -39,6 +63,8 @@ public class Controller {
 	}
 	
 	public static void main(String[] args) {
+		Controller controller = new Controller();
+		System.out.println(controller.decideOutfit());
 		
 	}
 	
