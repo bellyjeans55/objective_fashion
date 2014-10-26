249d0a37c76db01d232fbfee56dae10efecc0e8c
Jonathan Kay <bellyjeans55@gmail.com>
2014-10-25 22:22:01 -0400

    implemented color toString

--- objective_fashion/src/clothing/variables/Color.java
+++ objective_fashion/src/clothing/variables/Color.java
@@ -4,4 +4,45 @@ public enum Color {
 	BLACK, BLUE, BROWN, BURGUNDY, DARK_BLUE, DARK_GREEN, GREEN, 
 	GREY, LIGHT_BLUE, LIGHT_GREEN, ORANGE, PINK, PURPLE, RED, 
 	TAN, YELLOW, WHITE;
+	
+	public String toString() {
+		String colorString = "";
+		switch (this) {
+		case BLACK: colorString = "Black";
+			break;
+		case BLUE: colorString = "Blue";
+			break;
+		case BROWN: colorString = "Brown";
+			break;
+		case BURGUNDY: colorString = "Burgundy";
+			break;
+		case DARK_BLUE: colorString = "Dark blue";
+			break;
+		case DARK_GREEN: colorString = "Dark green";
+			break;
+		case GREEN: colorString = "Green";
+			break;
+		case GREY: colorString = "Grey";
+			break;
+		case LIGHT_BLUE: colorString = "Light blue";
+			break;
+		case LIGHT_GREEN: colorString = "Light green";
+			break;
+		case ORANGE: colorString = "Orange";
+			break;
+		case PINK: colorString = "Pink";
+			break;
+		case PURPLE: colorString = "Purple";
+			break;
+		case RED: colorString = "Red";
+			break;
+		case TAN: colorString = "Tan";
+			break;
+		case WHITE: colorString = "White";
+			break;
+		case YELLOW: colorString = "Yellow";
+			break;
+		}
+		return colorString;
+	}
 }
