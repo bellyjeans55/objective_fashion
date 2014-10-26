d369de69ac0ac018cb41f59375ce4e9fe8bc20a3
j5r5myk <j5r5myk@gmail.com>
2014-10-25 21:01:11 -0400

    weather class calculates temperature for chapel hill

--- objective_fashion/src/externalVariables/Weather.java
+++ objective_fashion/src/externalVariables/Weather.java
@@ -1,11 +1,26 @@
 package externalVariables;
 
+import de.mbenning.weather.wunderground.api.domain.WeatherStation;
+import de.mbenning.weather.wunderground.impl.services.HttpDataReaderService;
+
 public class Weather {
-	public int temperature;
+	public static int temperature;
 	
-	public Weather(int temperature) {
-		this.temperature = temperature;
+	public static void calculateWeather() {
+		// create a instance of a wunderground data reader
+		HttpDataReaderService dataReader = new HttpDataReaderService();
+
+		// select a wunderground weather station (ID "KMABOSTO32" = somewhere)
+		WeatherStation weatherStation = new WeatherStation("KNCCHAPE17");
+
+		// set selected weather station to data reader
+		dataReader.setWeatherStation(weatherStation);
+		
+		// set temperature
+		temperature = dataReader.getCurrentData().getTemperature().intValue();
+		
 	}
+	
 	
 	public int getTemperature() {
 		return temperature;
